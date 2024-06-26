9f88076f8ba5cd1e3c8c834f39db8e70feeee101
lazaro <lazaroalves12355@gmail.com>
2024-06-26 17:15:40 -0300

    ajustes

--- src/app/_components/Contact.tsx
+++ src/app/_components/Contact.tsx
@@ -11,19 +11,19 @@ export const Contact = () => {
                         Vamos iniciar essa jornada juntos. Entre em contato e vamos conversar
                         sobre seu projeto.</ContactSubTitle>
                     <ContactItens>
-                        <ContactLink href="https://github.com/lazaroalvesr">
+                        <ContactLink href="https://github.com/lazaroalvesr" target="_blank">
                             <DivIcons>
                                 <ContactIcons src="/img/Icons/Icone_GittHub.png" alt="Icone GitHUb" />
                                 <IconsText>https://github.com/lazaroalvesr</IconsText>
                             </DivIcons>
                         </ContactLink>
-                        <ContactLink href="e">
+                        <ContactLink href="/">
                             <DivIcons>
                                 <ContactIcons src="/img/Icons/Icone_Email.png" alt="Icone Email"/>
                                 <IconsText>lazaroalves12355@gmail.com</IconsText>
                             </DivIcons>
                         </ContactLink>
-                        <ContactLink href="e">
+                        <ContactLink href="https://www.linkedin.com/in/l%C3%A1zaro-alves-r/" target="_blank">
                             <DivIcons>
                                 <ContactIcons src="/img/Icons/Icone_Linkedin.png"  alt="Icone Linkedin"/>
                                 <IconsText>https://www.linkedin.com/in/l%C3%A1zaro-alves-r/</IconsText>
