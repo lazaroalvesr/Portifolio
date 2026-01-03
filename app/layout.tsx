37c55ca7bee25384c4c07a0a46d37fba0a1a871c
lazaro <lazaroalves12355@gmail.com>
2026-01-03 10:11:45 -0300

    feat: analystic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Work_Sans } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const workSans = Work_Sans({
   variable: "--font-work-sans",
